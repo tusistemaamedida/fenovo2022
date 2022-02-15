7d4f0c9586ac5d8182a1bbfd1701053d200aec98
Guillermo Albornoz <cachoalbornoz@gmail.com>
2022-02-15 19:58:00 -0300

    Fix Edit movimiento

--- app/Models/SessionProduct.php
+++ app/Models/SessionProduct.php
@@ -6,7 +6,6 @@
 
 namespace App\Models;
 
-
 use App\Traits\OriginDataTrait;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
@@ -14,23 +13,23 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * Class SessionProduct
  *
- * @property int $id
- * @property string $list_id
- * @property int|null $store_id
+ * @property int         $id
+ * @property string      $list_id
+ * @property int|null    $store_id
  * @property string|null $movement_id
- * @property int|null $product_id
+ * @property int|null    $product_id
  * @property string|null $product_name
  * @property string|null $unit_type
- * @property float|null $unit_price
- * @property float|null $tasiva
- * @property int|null $senasa_id
+ * @property float|null  $unit_price
+ * @property float|null  $tasiva
+ * @property int|null    $senasa_id
  * @property string|null $senasa_name
  * @property string|null $unit_package
- * @property float|null $quantity
+ * @property float|null  $quantity
  * @property string|null $state
- * @property bool|null $invoice
- * @property float|null $net_weight
- * @property float|null $gross_weight
+ * @property bool|null   $invoice
+ * @property float|null  $net_weight
+ * @property float|null  $gross_weight
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  *
@@ -39,32 +38,33 @@ use Illuminate\Database\Eloquent\Model;
 class SessionProduct extends Model
 {
     use OriginDataTrait;
-	protected $table = 'session_products';
+    protected $table = 'session_products';
 
-	protected $casts = [
-		'store_id' => 'int',
-		'product_id' => 'int',
-		'unit_price' => 'float',
-		'tasiva' => 'float',
-		'senasa_id' => 'int',
-		'quantity' => 'float',
-		'invoice' => 'bool',
-		'net_weight' => 'float',
-		'gross_weight' => 'float'
-	];
+    protected $casts = [
+        'store_id'     => 'int',
+        'product_id'   => 'int',
+        'unit_price'   => 'float',
+        'tasiva'       => 'float',
+        'senasa_id'    => 'int',
+        'quantity'     => 'float',
+        'invoice'      => 'bool',
+        'net_weight'   => 'float',
+        'gross_weight' => 'float',
+    ];
 
-	protected $fillable = [
-		'list_id',
-		'store_id',
-		'product_id',
-		'unit_price',
+    protected $fillable = [
+        'list_id',
+        'store_id',
+        'product_id',
+        'unit_price',
         'tasiva',
-		'unit_package',
-		'quantity',
-		'invoice'
-	];
+        'unit_package',
+        'quantity',
+        'invoice',
+    ];
 
-    public function producto(){
+    public function producto()
+    {
         return $this->belongsTo(Product::class, 'product_id', 'id');
     }
 }
--- routes/admin/movimientos/salidas.php
+++ routes/admin/movimientos/salidas.php
@@ -12,8 +12,12 @@ Route::get('salidas-pendiente/show', [SalidasController::class, 'pendienteShow']
 Route::get('clientes/salidas', [SalidasController::class, 'getClienteSalida'])->name('get.cliente.salida');
 Route::get('search-products', [SalidasController::class, 'searchProducts'])->name('search.products');
 Route::get('session-products-by-list-id', [SalidasController::class, 'getSessionProducts'])->name('get.session.products');
+Route::get('flete-by-list-id', [SalidasController::class, 'getFleteSessionProducts'])->name('get.flete.session.products');
+
 Route::post('delete-session-product', [SalidasController::class, 'deleteSessionProduct'])->name('delete.item.session.produc');
 Route::post('store-session-product', [SalidasController::class, 'storeSessionProduct'])->name('store.session.product');
+Route::post('store-session-product-item', [SalidasController::class, 'storeSessionProductItem'])->name('store.session.product.item');
+
 Route::post('guardar-salida', [SalidasController::class, 'storeSalida'])->name('guardar.salida');
 Route::post('cambiar-facturacion', [SalidasController::class, 'changeInvoiceProduct'])->name('change.invoice.product');
 Route::get('get-presentaciones', [SalidasController::class, 'getPresentaciones'])->name('get.presentaciones');
