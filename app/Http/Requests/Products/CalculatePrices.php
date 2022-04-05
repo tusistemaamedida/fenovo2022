4dddc37a02ca8f3b8144e11f742cc0920544c1c7
SDEV - Iván D. Fontana <ivan@sistemasdev.com>
2022-04-05 14:04:49 -0300

    btn prices

--- app/Http/Requests/Products/CalculatePrices.php
+++ app/Http/Requests/Products/CalculatePrices.php
@@ -19,14 +19,14 @@ class CalculatePrices extends FormRequest
     public function rules(){
         return [
             'plistproveedor'=> 'required|numeric',
-            'descproveedor' => 'nullable|numeric|max:100',
-            'mupfenovo'     => 'nullable|numeric|max:100',
-            'contribution_fund' => 'nullable|numeric|max:100',
+            'descproveedor' => 'nullable|numeric|max:99.9',
+            'mupfenovo'     => 'nullable|numeric|max:99.9',
+            'contribution_fund' => 'nullable|numeric|max:99.9',
             'tasiva'    => 'nullable|numeric',
-            'muplist2'  => 'nullable|numeric|max:100',
-            'muplist2'  => 'nullable|numeric|max:100',
+            'muplist2'  => 'nullable|numeric|max:99.9',
+            'muplist2'  => 'nullable|numeric|max:99.9',
             'p1tienda'  => 'nullable|numeric',
-            'descp1'    => 'nullable|numeric|max:100',
+            'descp1'    => 'nullable|numeric|max:99.9',
             'p2tienda'  => 'nullable|numeric',
             'descp2'    => 'nullable|numeric',
         ];
@@ -37,26 +37,26 @@ class CalculatePrices extends FormRequest
             'plistproveedor.required'   => 'El precio proveedor es requerido!',
             'plistproveedor.numeric'    => 'El precio proveedor debe ser numérico!',
             'descproveedor.numeric'     => 'El desc. proveedor debe ser numérico!',
-            'descproveedor.max'         => 'El desc. proveedor debe ser maximo 100%!',
+            'descproveedor.max'         => 'El desc. proveedor debe ser menor a 100%!',
             'mupfenovo.numeric'         => 'El Markup Fenovo debe ser numérico!',
-            'mupfenovo.max'         => 'El Markup Fenovo debe ser maximo 100%!',
+            'mupfenovo.max'         => 'El Markup Fenovo debe ser menor a 100%!',
             'contribution_fund.numeric' => 'El Fondo contribución debe ser numérico!',
 
-            'contribution_fund.max' => 'El Fondo contribución debe ser maximo 100%!',
+            'contribution_fund.max' => 'El Fondo contribución debe ser menor a 100%!',
 
             'tasiva.numeric'            => 'El IVA debe ser numérico!',
             'muplist1.numeric'          => 'El Markup lista 1 debe ser numérico!',
             'muplist2.numeric'          => 'El Markup lista 2 debe ser numérico!',
 
-            'muplist1.max'          => 'El Markup lista 1 debe ser maximo 100%!',
-            'muplist2.max'          => 'El Markup lista 2 debe ser maximo 100%!',
+            'muplist1.max'          => 'El Markup lista 1 debe ser menor a 100%!',
+            'muplist2.max'          => 'El Markup lista 2 debe ser menor a 100%!',
 
             'p1tienda.numeric'          => 'El Precio tienda 1 debe ser numérico!',
             'descp1.numeric'            => 'El Desc. mayorista debe ser numérico!',
             'p2tienda.numeric'          => 'El Precio tienda 2 debe ser numérico!',
             'descp2.numeric'            => 'El Desc. mayorista debe ser numérico!',
 
-            'descp1.max'            => 'El Desc. mayorista debe ser maximo 100%!',
+            'descp1.max'            => 'El Desc. mayorista debe ser menor a 100%!',
             //'descp2.max'            => 'El Desc. mayorista debe ser maximo 100%!',
         ];
     }
