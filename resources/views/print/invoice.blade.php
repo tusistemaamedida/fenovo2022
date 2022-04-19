4f01de9fa5d3e76b2326e80748540ab43e91efd5
SDEV - Iván D. Fontana <ivan@sistemasdev.com>
2022-04-19 12:45:57 -0300

    pruebas

--- resources/views/print/invoice.blade.php
+++ resources/views/print/invoice.blade.php
@@ -174,7 +174,7 @@
                         <span style="font-size: 18px;">
                           {{$titulo}} <br>
                           N°: {{ $invoice->voucher_number }} <br>
-                          Fecha: {{\Carbon\Carbon::parse(now())->format('d/m/y H:i')}}
+                          Fecha: {{\Carbon\Carbon::parse($invoice->created_at)->format('d/m/y H:i')}}
                           <!-- fecha de impresion -->
                         </span>
                       </th>
