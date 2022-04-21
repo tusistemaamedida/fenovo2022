c4cf2a083c2f746af012fb2427bda67704df4cfc
SDEV - Iván D. Fontana <ivan@sistemasdev.com>
2022-04-21 11:35:59 -0300

    avances

--- app/Http/Controllers/Admin/Movimientos/SalidasController.php
+++ app/Http/Controllers/Admin/Movimientos/SalidasController.php
@@ -218,7 +218,7 @@ class SalidasController extends Controller
     {
         $movement = Movement::query()->where('id', $request->id)->with('panamas')->first();
         if ($movement) {
-            $id_panama = 'P'.str_pad($movement->id, 8, "0", STR_PAD_LEFT);
+            $id_panama = '8888'.str_pad($movement->orden, 8, "0", STR_PAD_LEFT);
             $destino         = $this->origenData($movement->type, $movement->to, true);
             $neto            = 0;
             $array_productos = [];
