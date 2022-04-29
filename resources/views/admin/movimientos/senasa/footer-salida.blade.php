e35e4b318a4bff97fbbb6dd9d4d2b014023d5bb4
Guillermo Albornoz <cachoalbornoz@gmail.com>
2022-04-29 14:35:51 -0300

    Fix SENASA Report - C

--- resources/views/admin/movimientos/senasa/footer-salida.blade.php
+++ resources/views/admin/movimientos/senasa/footer-salida.blade.php
@@ -2,19 +2,19 @@
     <table style="width:100%; margin-top:2cm; font-size: 9px ">
         <tr>
             <td colspan="2">
-                <strong style="margin-left: 4.2cm">{{ $senasa->patente_nro }}</strong> 
-                <strong style="margin-left: 5cm">{{ $senasa->habilitacion_nro }}</strong> 
+                <strong style="margin-left: 4.2cm">{{ $senasa->patente_nro }}</strong>
+                <strong style="margin-left: 5cm">{{ $senasa->habilitacion_nro }}</strong>
                 <strong style="margin-left: 4.5cm">{{ $senasa->precintos }}</strong>
             </td>
         </tr>
         <tr>
             <td colspan="2">
-                <strong>{{ $senasa->destino }}</strong>
+                <strong style="margin-left: 0.5cm">{{ $senasa->destino }}</strong>
             </td>
         </tr>
         <tr class="text-center">
             <td colspan="2">
-                <strong style="margin-left: 8.5cm; margin-top:1cm"> {{ $senasa->dias_validez }} </strong>
+                <strong style="margin-left: 7.5cm; margin-top:1cm"> {{ $senasa->dias_validez }} </strong>
             </td>
         </tr>
     </table>
