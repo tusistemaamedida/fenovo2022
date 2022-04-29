4bcebab0c8953e249bb084778ab68a66e7a52773
Guillermo Albornoz <cachoalbornoz@gmail.com>
2022-04-29 13:54:07 -0300

    Fix SENASA Report - A

--- resources/views/admin/movimientos/senasa/footer-salida.blade.php
+++ resources/views/admin/movimientos/senasa/footer-salida.blade.php
@@ -1,9 +1,9 @@
 <footer-5>
-    <table style="width:100%; font-size: 9px ">
+    <table style="width:100%; margin-top:0.5cm; font-size: 9px ">
         <tr>
             <td colspan="2">
-                <strong style="margin-left: 4cm">{{ $senasa->patente_nro }}</strong> 
-                <strong style="margin-left: 4.5cm">{{ $senasa->habilitacion_nro }}</strong> 
+                <strong style="margin-left: 4.2cm">{{ $senasa->patente_nro }}</strong> 
+                <strong style="margin-left: 5cm">{{ $senasa->habilitacion_nro }}</strong> 
                 <strong style="margin-left: 4.5cm">{{ $senasa->precintos }}</strong>
             </td>
         </tr>
@@ -14,7 +14,7 @@
         </tr>
         <tr class="text-center">
             <td colspan="2">
-                <strong style="margin-left: 6cm; margin-top:1cm"> {{ $senasa->dias_validez }} </strong>
+                <strong style="margin-left: 11cm; margin-top:1cm"> {{ $senasa->dias_validez }} </strong>
             </td>
         </tr>
     </table>
