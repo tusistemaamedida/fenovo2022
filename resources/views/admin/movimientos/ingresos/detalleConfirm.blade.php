973ae21954684e0b2dfbb061bf80ddcc7d56287b
Guillermo Albornoz <cachoalbornoz@gmail.com>
2022-05-09 12:27:14 -0300

    Fix guardar Costo Fenovo en la Compra

--- resources/views/admin/movimientos/ingresos/detalleConfirm.blade.php
+++ resources/views/admin/movimientos/ingresos/detalleConfirm.blade.php
@@ -23,7 +23,7 @@
             @foreach ($movimientos as $movimiento)
 
             @php
-                $total = $total+($movimiento->product->product_price->costfenovo*$movimiento->unit_package*$movimiento->bultos);
+                $total = $total+($movimiento->cost_fenovo*$movimiento->unit_package*$movimiento->bultos);
             @endphp
 
             <tr>
@@ -34,7 +34,7 @@
                 <td class="text-center"> {{ number_format($movimiento->unit_package,2) }} </td>
                 <td class="text-center"> {{ $movimiento->cost_fenovo }}</td>
                 <td class="text-center"> {{ $movimiento->bultos }}</td>
-                <td class="text-center"> {{ number_format($movimiento->product->product_price->costfenovo*$movimiento->unit_package*$movimiento->bultos,2, ',', '.') }}</td>
+                <td class="text-center"> {{ number_format($movimiento->cost_fenovo*$movimiento->unit_package*$movimiento->bultos,2, ',', '.') }}</td>
                 <td class="text-center"> {{ $movimiento->entry }} </td>
                 <td> <a href="#" onclick="borrarDetalle('{{ $movimiento->movement_id}}', '{{ $movimiento->product_id }}')"><i class="fa fa-trash"></i></a> </td>
                 <td> </td>
