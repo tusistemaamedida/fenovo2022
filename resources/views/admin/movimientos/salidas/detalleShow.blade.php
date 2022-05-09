a76f17effefb0472c364d7896b6b8b2f419e066e
Guillermo Albornoz <cachoalbornoz@gmail.com>
2022-05-09 11:48:45 -0300

    Fix export movimientos - Kilos y Unidades

--- resources/views/admin/movimientos/salidas/detalleShow.blade.php
+++ resources/views/admin/movimientos/salidas/detalleShow.blade.php
@@ -3,58 +3,64 @@
 <div class="table-datapos">
     <div class="table-responsive">
 
-<table class="table table-hover table-sm text-center yajra-datatable" id="show-salida">
-    <tr class=" bg-dark text-white">
-        <th>#</th>
-        <th>Cod fenovo</th>
-        <th>Producto</th>
-        <th>Presentación</th>
-        <th>Subtotal</th>
-        <th>Bultos</th>
-        <th>Kilos</th>
-    </tr>
+        <table class="table table-hover table-sm text-center yajra-datatable" id="show-salida">
+            <tr class=" bg-dark text-white">
+                <th>#</th>
+                <th>Cod fenovo</th>
+                <th>Producto</th>
+                <th>Presentación</th>
+                <th>Unidad</th>
+                <th>Subtotal</th>
+                <th>Bultos</th>
+                <th>Cantidad</th>
+            </tr>
 
-    @php
-    $subtotal = 0;
-    @endphp
+            @php
+            $subtotal = 0;
+            @endphp
 
-    @foreach ($movement->movement_salida_products as $movimiento)
-    <tr>
-        <td> {{ $loop->iteration }}</td>
-        <td> {{ $movimiento->product->cod_fenovo }} </td>
-        <td> {{ $movimiento->product->name }}</td>
-        <td> {{ $movimiento->unit_package }} </td>
-        <td> {{ $movimiento->unit_price*$movimiento->bultos*$movimiento->unit_package }} </td>
-        <td> {{ $movimiento->bultos }}</td>
-        <td> {{ number_format($movimiento->egress,2) }} </td>
-    </tr>
-    @php
-    $subtotal = $subtotal + ($movimiento->unit_price*$movimiento->bultos*$movimiento->unit_package);
-    @endphp
+            @foreach ($movement->movement_salida_products as $movimiento)
+            <tr>
+                <td> {{ $loop->iteration }}</td>
+                <td> {{ $movimiento->product->cod_fenovo }} </td>
+                <td> {{ $movimiento->product->name }}</td>
+                <td> {{ $movimiento->unit_package }} </td>
+                <td> {{ $movimiento->product->unit_type }} </td>
+                <td> {{ $movimiento->unit_price*$movimiento->bultos*$movimiento->unit_package }} </td>
+                <td> {{ $movimiento->bultos }}</td>
+                <td>
+                    {{ ($movimiento->product->unit_type == 'K')?$movimiento->egress:$movimiento->bultos*$movimiento->unit_package}}
+                </td>
+            </tr>
+            @php
+            $subtotal = $subtotal + ($movimiento->unit_price*$movimiento->bultos*$movimiento->unit_package);
+            @endphp
 
-    @endforeach
-    <tr>
-        <th colspan="7"><br></th>
-    </tr>
-    <tr class=" bg-info text-white">
-        <th></th>
-        <th>TOTALES</th>
-        <th></th>
-        <th></th>
-        <th>{{ $subtotal }}</th>
-        <th>{{ number_format($movement->movement_salida_products->sum('bultos'),2) }}</th>
-        <th>{{ number_format($movement->movement_salida_products->sum('egress'),2) }}</th>
-    </tr>
-    <tr>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th>FLETE ( {{ number_format($store->delivery_percentage,2) }} %)</th>
-        <th>{{ number_format($movement->flete, 2) }}</th>
-        <th></th>
-        <th></th>
-    </tr>
-</table>
+            @endforeach
+            <tr>
+                <th colspan="8"><br></th>
+            </tr>
+            <tr class=" bg-info text-white">
+                <th></th>
+                <th>TOTALES</th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th>{{ $subtotal }}</th>
+                <th>{{ number_format($movement->movement_salida_products->sum('bultos'),2) }}</th>
+                <th>{{ number_format($movement->movement_salida_products->sum('egress'),2) }} Kgrs</th>
+            </tr>
+            <tr>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th>FLETE ( {{ number_format($store->delivery_percentage,2) }} %)</th>
+                <th>{{ number_format($movement->flete, 2) }}</th>
+                <th></th>
+                <th></th>
+            </tr>
+        </table>
 
     </div>
 </div>
