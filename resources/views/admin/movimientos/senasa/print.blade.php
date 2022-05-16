2d246ab169ee7f38ecf206b03597df4debb85106
Guillermo Albornoz <cachoalbornoz@gmail.com>
2022-05-16 09:50:40 -0300

    Fix salidas senasa

--- resources/views/admin/movimientos/senasa/print.blade.php
+++ resources/views/admin/movimientos/senasa/print.blade.php
@@ -10,41 +10,63 @@
 
 @include('admin.movimientos.senasa.encabezado')
 
-    @foreach($movimientos as $movimiento)
-    <tr>
-        <td class="text-center">{{ $movimiento->bultos }}</td>
-        <td>{{ $movimiento->name }}</td>
-        <td class="text-center">{{ number_format($movimiento->kilos,2) }}</td>
-        <td class="text-center"></td>
-        <td class="text-center"></td>
-    </tr>
 
-        <!-- Revisa si al imprimir el detalle, supera la linea 28 -->
-        @if ($loop->iteration % 28 == 0)
+@php
+$total_k = 0;
+$total_ku = 0;
+@endphp
 
-        </table>
+@foreach($movimientos as $movimiento)
 
-        <div class="page-break"></div>
-
-        @include('admin.movimientos.senasa.encabezado')
+@php
+if($movimiento->unit_type == 'K'){
+$total_k += $movimiento->kilos;
+}else{
+$total_ku += $movimiento->egress*$movimiento->unit_weight;
+}
+@endphp
 
 
+<tr>
+    <td class="text-center">{{ $movimiento->bultos }}</td>
+    <td>{{ $movimiento->name }}</td>
+    <td class="text-center">
+        @if ($movimiento->unit_type == 'K')
+        {{ number_format($movimiento->kilos,2) }}
+        @else
+        {{ number_format($movimiento->egress*$movimiento->unit_weight,2) }}
         @endif
+    </td>
+    <td class="text-center"></td>
+    <td class="text-center"></td>
+</tr>
+
+<!-- Revisa si al imprimir el detalle, supera la linea 28 -->
+@if ($loop->iteration % 30 == 0)
+
+</table>
+
+<div class="page-break"></div>
+
+@include('admin.movimientos.senasa.encabezado')
 
 
-    @endforeach
-    <tr>
-        <td colspan="5">
-            &nbsp;
-        </td>
-    </tr>
-    <tr>
-        <th class="text-center"> {{ number_format($movimientos->sum('bultos')) }}</strong> </th>
-        <td>&nbsp;</td>
-        <td class="text-center"> <strong> {{ number_format($movimientos->sum('kilos'),2) }} Kgrs</strong></td>
-        <td class="text-center"> </td>
-        <td class="text-center"> </td>
-    </tr>
+@endif
+
+
+@endforeach
+<tr>
+    <td colspan="5">
+        &nbsp;
+    </td>
+</tr>
+<tr>
+    <th class="text-center"> {{ number_format($movimientos->sum('bultos')) }}</strong> </th>
+    <td>&nbsp;</td>
+    <td class="text-center"> <strong> {{ number_format($total_k+$total_ku,2) }} Kgrs</strong></td>
+    <td class="text-center"> </td>
+    <td class="text-center"> </td>
+</tr>
 </table>
 
 @include('admin.movimientos.senasa.footer-salida')
