a8ef37d05ec66ae7973a3a6ebf2243c9b8e79fc1
Guillermo Albornoz <cachoalbornoz@gmail.com>
2022-05-23 18:45:26 -0300

    Fix preparar salidas - DataTable Scroll Y

--- app/Http/Requests/Proveedors/StoreRequest.php
+++ app/Http/Requests/Proveedors/StoreRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Proveedors;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class EditRequest extends FormRequest
+class StoreRequest extends FormRequest
 {
     /**
      * @return bool
@@ -17,7 +17,7 @@ class EditRequest extends FormRequest
     public function rules()
     {
         return [
-            'cuit' => 'required|string|min:11|max:11|cuit|unique:proveedors,cuit,' . $this->proveedor_id,
+            'cuit' => 'required|string|min:11|max:11|cuil|unique:proveedors',
             'name' => 'required',
         ];
     }
@@ -26,8 +26,7 @@ class EditRequest extends FormRequest
     {
         return [
             'cuit.required' => 'El cuit es requerido!',
-            'cuit.cuil'     => 'El nro de cuit no es válido !',
-            'cuit.unique'   => 'El cuit se encuentra repetido !',
+            'cuil.cuil'     => 'El nro de cuit no es válido !',
             'name.required' => 'Razon social es requerida',
         ];
     }
