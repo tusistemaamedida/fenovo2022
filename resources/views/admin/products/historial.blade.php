007b1a684080a93a1fbd62db35007b0a80ba03c2
Guillermo Albornoz <cachoalbornoz@gmail.com>
2022-05-24 13:59:39 -0300

    Fix update stock

--- resources/views/admin/products/historial.blade.php
+++ resources/views/admin/products/historial.blade.php
@@ -32,7 +32,6 @@
                                 <table class=" table table-hover display dataTable yajra-datatable">
                                     <thead class="text-body">
                                         <tr class="bg-light">
-                                            <th>#MovId</th>
                                             <th>Fecha</th>
                                             <th>Tipo</th>
                                             <th>Desde</th>
@@ -68,7 +67,6 @@
         autoWidth: false,
         ajax: '{{ route('product.historial', ['id' => $producto->id] ) }}',
         columns: [
-            {data: 'id', 'class':'text-center', orderable: false, searchable: false},
             {data: 'fecha', 'class':'text-center', orderable: false, searchable: false},
             {data: 'type', 'class':'text-left', orderable: false, searchable: true},
             {data: 'from', 'class':'text-left', orderable: false, searchable: true},
