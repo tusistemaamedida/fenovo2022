c4a19bebfd7a4a15d2d031a4fe8dc44498f2b017
Guillermo Albornoz <cachoalbornoz@gmail.com>
2022-06-10 10:04:35 -0300

    NavBar menues

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -216,3 +216,31 @@ jQuery('.show-t').on('click', function () {
         },
     });
 })(jQuery);
+
+// BARRA NAVEGACION / SUBMENUES
+
+const $dropdown = jQuery(".dropdown");
+const $dropdownToggle = jQuery(".dropdown-toggle");
+const $dropdownMenu = jQuery(".dropdown-menu");
+const showClass = "show";
+ 
+jQuery(window).on("load resize", function() {
+  if (this.matchMedia("(min-width: 768px)").matches) {
+    $dropdown.hover(
+      function() {
+        const $this = jQuery(this);
+        $this.addClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "true");
+        $this.find($dropdownMenu).addClass(showClass);
+      },
+      function() {
+        const $this = jQuery(this);
+        $this.removeClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "false");
+        $this.find($dropdownMenu).removeClass(showClass);
+      }
+    );
+  } else {
+    $dropdown.off("mouseenter mouseleave");
+  }
+});
