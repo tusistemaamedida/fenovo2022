860551bdb56964282cf6bc31824a4d95245e5ab9
Guillermo Albornoz <cachoalbornoz@gmail.com>
2022-06-16 10:32:00 -0300

    Fix Nota credito

--- app/Http/Controllers/Admin/Movimientos/SalidasController.php
+++ app/Http/Controllers/Admin/Movimientos/SalidasController.php
@@ -751,9 +751,9 @@ class SalidasController extends Controller
             $excepcion = false;
             // busco el producto en session oferta ordenados asc para tomar el primero
             $session_oferta = SessionOferta::where('fecha_desde', '<=', Carbon::parse(now())->format('Y-m-d'))
-                                            ->where('product_id', $product_id)
-                                            ->orderBy('fecha_hasta', 'ASC')
-                                            ->first();
+                ->where('product_id', $product_id)
+                ->orderBy('fecha_hasta', 'ASC')
+                ->first();
 
             if ($session_oferta) {
                 // si existe una oferta busco si esa oferta es una excepcion
