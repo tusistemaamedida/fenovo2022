24348985d933735d24466c401f1ba4033906ef36
Guillermo Albornoz <cachoalbornoz@gmail.com>
2022-07-29 12:52:35 -0300

    Fix Header - Permission Header

--- resources/views/admin/permissions/index.blade.php
+++ resources/views/admin/permissions/index.blade.php
@@ -2,54 +2,46 @@
 
 @section('content')
 
-<div class="d-flex flex-column-fluid">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                <div class="row">
-                    <div class="col-lg-12 col-xl-12">
-                        <div class="card card-custom gutter-b bg-transparent shadow-none border-0">
-                            <div class="card-header align-items-center  border-bottom-dark px-0">
-                                <div class="card-title mb-0">
-                                    <h4 class="card-label mb-0 font-weight-bold text-body">
-                                        Permisos
-                                    </h4>
-                                </div>
-                                <div class="icons d-flex">
-                                    <a href="javascript:void(0)" onclick="add('{{ route('permissions.add') }}')" class="ml-2">
-                                        <i class="fa fa-2x fa-plus-circle text-primary"></i>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
+
+    <div class="row">
+        <div class="col-12">
+            <div class="card card-custom gutter-b bg-transparent shadow-none border-0">
+                <div class="card-header align-items-center  border-bottom-dark">
+                    <div class="card-title mb-0">
+                        <h4 class="card-label mb-0 font-weight-bold text-body">
+                            Permisos
+                        </h4>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-12 ">
-                        <div class="card card-custom gutter-b bg-white border-0">
-                            <div class="card-body">
-                                <table class="display table-hover yajra-datatable">
-                                    <thead>
-                                        <tr class="bg-dark text-white">
-                                            <th>No</th>
-                                            <th>Nombre</th>
-                                            <th>Rol</th>
-                                            <th></th>
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+                    <div class="icons d-flex">
+                        <a href="javascript:void(0)" onclick="add('{{ route('permissions.add') }}')">
+                            <i class="fa fa-2x fa-plus-circle text-primary"></i>
+                        </a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-</div>
+    <div class="row">
+        <div class="col-12">
+            <div class="card card-custom gutter-b bg-white border-0">
+                <div class="card-body">
+                    <table class="display table-hover yajra-datatable">
+                        <thead>
+                            <tr class="bg-dark text-white">
+                                <th>No</th>
+                                <th>Nombre</th>
+                                <th>Rol</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @include('admin.permissions.modal')
 
